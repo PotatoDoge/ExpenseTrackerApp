6e7bbb7d6c2585756efb3b58740cec62c9b98258
Bastian Prado <bastian.prado.v@gmail.com>
2025-04-21 14:38:10 -0700

    Add ExpenseEntity (database entity based on POJO)

---
+++ src/main/java/com/expensetrackerapp/infrastructure/outbound/entities/ExpenseEntity.java
@@ -0,0 +1,36 @@
+package com.expensetrackerapp.infrastructure.outbound.entities;
+
+import com.expensetrackerapp.domain.enums.PaymentMethod;
+import com.expensetrackerapp.domain.enums.RecurrenceType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+@Entity
+@Table(name = "expenses")
+@Data
+public class ExpenseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal amount;
+    private Currency currency;
+    private LocalDateTime dateTime;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+    private boolean requiresInvoice;
+    private boolean isPaidInFull;
+    private int installments;
+    private boolean isRecurring;
+    @Enumerated(EnumType.STRING)
+    private RecurrenceType recurrenceType;
+    private String vendor;
+    private String location;
+
+    // TODO: Relationships for card, category, tags, attachments, etc
+}
