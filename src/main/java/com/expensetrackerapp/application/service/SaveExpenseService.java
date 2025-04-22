5cdf04ddc2c72e9bf84b1900496b8819e5f54156
Bastian Prado <bastian.prado.v@gmail.com>
2025-04-21 21:31:10 -0700

    Add null validation

--- src/main/java/com/expensetrackerapp/application/service/SaveExpenseService.java
+++ src/main/java/com/expensetrackerapp/application/service/SaveExpenseService.java
@@ -7,9 +7,12 @@ import com.expensetrackerapp.domain.model.Expense;
 import com.expensetrackerapp.dto.ExpenseDTO;
 import com.expensetrackerapp.infrastructure.outbound.entities.ExpenseEntity;
 import com.expensetrackerapp.infrastructure.outbound.mappers.ExpenseMapper;
+import com.expensetrackerapp.shared.exceptions.NullRequestException;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -21,6 +24,9 @@ public class SaveExpenseService implements SaveExpenseUseCase<ExpenseDTO> {
 
     @Override
     public ExpenseDTO saveExpense(SaveExpenseRequest saveExpenseRequest) {
+        if (Objects.isNull(saveExpenseRequest)) {
+            throw new NullRequestException("Request's body (SaveExpenseRequest) cannot be null");
+        }
         Expense expense = expenseMapper.fromRequestToPojo(saveExpenseRequest);
         log.info("Saving expense: {}", expense);
         ExpenseEntity expenseEntity = saveExpensePort.saveExpense(expense);
