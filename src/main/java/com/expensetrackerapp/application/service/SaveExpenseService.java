4fb4a57ae4c22efa42307b7b9ad7b5b0b083597f
Bastian Prado <bastian.prado.v@gmail.com>
2025-04-26 23:39:45 -0700

    Add exception handling

--- src/main/java/com/expensetrackerapp/application/service/SaveExpenseService.java
+++ src/main/java/com/expensetrackerapp/application/service/SaveExpenseService.java
@@ -7,9 +7,13 @@ import com.expensetrackerapp.domain.model.Expense;
 import com.expensetrackerapp.dto.ExpenseDTO;
 import com.expensetrackerapp.infrastructure.outbound.entities.ExpenseEntity;
 import com.expensetrackerapp.infrastructure.outbound.mappers.ExpenseMapper;
+import com.expensetrackerapp.shared.exceptions.DatabaseInteractionException;
+import com.expensetrackerapp.shared.exceptions.MappingException;
 import com.expensetrackerapp.shared.exceptions.NullRequestException;
+import jakarta.persistence.PersistenceException;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -25,11 +29,28 @@ public class SaveExpenseService implements SaveExpenseUseCase<ExpenseDTO> {
     @Override
     public ExpenseDTO saveExpense(SaveExpenseRequest saveExpenseRequest) {
         if (Objects.isNull(saveExpenseRequest)) {
+            log.error("Request's body (SaveExpenseRequest) cannot be null");
             throw new NullRequestException("Request's body (SaveExpenseRequest) cannot be null");
         }
-        Expense expense = expenseMapper.fromRequestToPojo(saveExpenseRequest);
-        log.info("Saving expense: {}", expense);
-        ExpenseEntity expenseEntity = saveExpensePort.saveExpense(expense);
-        return expenseMapper.fromEntityToDTO(expenseEntity);
+
+        Expense expense;
+        try{
+            expense = expenseMapper.fromRequestToPojo(saveExpenseRequest);
+            log.info("Saving expense: {}", expense);
+            ExpenseEntity expenseEntity = saveExpensePort.saveExpense(expense);
+            return expenseMapper.fromEntityToDTO(expenseEntity);
+        }
+        catch (IllegalArgumentException | PersistenceException | DataAccessException |
+               NullPointerException | ClassCastException e) {
+            log.error("Error occurred while saving expense: {}", e.getClass().getSimpleName(), e);
+            throw new DatabaseInteractionException("Error while saving expense: " + e.getClass().getSimpleName());
+        }
+        catch (MappingException e) {
+            throw new MappingException("Error while mapping expense: " + e.getClass().getSimpleName());
+        }
+        catch (Exception e) {
+            log.error("Unexpected error occurred while saving expense", e);
+            throw new DatabaseInteractionException("Unhandled error while saving expensea.");
+        }
     }
 }
--- src/main/java/com/expensetrackerapp/infrastructure/outbound/adapters/GetExpensesRepository.java
+++ src/main/java/com/expensetrackerapp/infrastructure/outbound/adapters/GetExpensesRepository.java
@@ -4,9 +4,13 @@ import com.expensetrackerapp.application.port.in.GetExpenses.GetExpensesFilters;
 import com.expensetrackerapp.application.port.out.GetExpensesOutboundPort;
 import com.expensetrackerapp.infrastructure.outbound.entities.ExpenseEntity;
 import com.expensetrackerapp.infrastructure.outbound.repositories.ExpenseRepository;
+import com.expensetrackerapp.shared.exceptions.DatabaseInteractionException;
+import jakarta.persistence.PersistenceException;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 
@@ -22,23 +26,29 @@ public class GetExpensesRepository implements GetExpensesOutboundPort<ExpenseEnt
         try {
             List<ExpenseEntity> expenseEntities =
                     expenseRepository.findAllExpensesByFilters(
-                                    filters.getExpenseId(),
-                                    filters.getExpenseName(),
-                                    filters.getDescription(),
-                                    filters.getAmount(),
-                                    filters.getCurrency(),
-                                    filters.getExpenseDate(),
-                                    filters.getRequiresInvoice(),
-                                    filters.getInstallments(),
-                                    filters.getVendor(),
-                                    filters.getVendor(),
-                                    filters.getPaidInFull());
+                            filters.getExpenseId(),
+                            filters.getExpenseName(),
+                            filters.getDescription(),
+                            filters.getAmount(),
+                            filters.getCurrency(),
+                            filters.getExpenseDate(),
+                            filters.getRequiresInvoice(),
+                            filters.getInstallments(),
+                            filters.getVendor(),
+                            filters.getLocation(),
+                            filters.getPaidInFull());
             log.info("Fetched expenses: {}", expenseEntities.size());
             return expenseEntities;
         }
-        catch (Exception e) {
-            log.error("Error while fetching expenses", e);
-            throw e;
+        catch (IllegalArgumentException | PersistenceException | DataAccessException |
+               NullPointerException | ClassCastException e) {
+            log.error("Error occurred while fetching expenses: {}", e.getClass().getSimpleName(), e);
+            throw new DatabaseInteractionException("Error while fetching expenses: " + e.getClass().getSimpleName());
         }
+        catch (Exception e) {
+            log.error("Unexpected error occurred while fetching expenses", e);
+            throw new DatabaseInteractionException("Unhandled error while fetching all entities.");
+        }
+
     }
 }
