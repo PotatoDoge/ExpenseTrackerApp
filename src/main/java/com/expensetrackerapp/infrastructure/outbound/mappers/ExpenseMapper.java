2b67833debc5acdc9ba6bbd1cd1551be3073dc7d
Bastian Prado <bastian.prado.v@gmail.com>
2025-04-26 23:37:02 -0700

    Add exception handling for fromRequestToPojo method

--- src/main/java/com/expensetrackerapp/infrastructure/outbound/mappers/ExpenseMapper.java
+++ src/main/java/com/expensetrackerapp/infrastructure/outbound/mappers/ExpenseMapper.java
@@ -4,8 +4,11 @@ import com.expensetrackerapp.application.port.in.SaveExpense.SaveExpenseRequest;
 import com.expensetrackerapp.domain.model.Expense;
 import com.expensetrackerapp.dto.ExpenseDTO;
 import com.expensetrackerapp.infrastructure.outbound.entities.ExpenseEntity;
+import com.expensetrackerapp.shared.exceptions.MappingException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class ExpenseMapper implements ExtendedMapper<Expense, ExpenseEntity, ExpenseDTO, SaveExpenseRequest> {
 
@@ -50,24 +53,30 @@ public class ExpenseMapper implements ExtendedMapper<Expense, ExpenseEntity, Exp
 
     @Override
     public Expense fromRequestToPojo(SaveExpenseRequest saveExpenseRequest) {
-        return Expense.builder()
-                .name(saveExpenseRequest.getName())
-                .description(saveExpenseRequest.getDescription())
-                .amount(saveExpenseRequest.getAmount())
-                .currency(saveExpenseRequest.getCurrency())
-                .expenseDate(saveExpenseRequest.getExpenseDate())
-                .paymentMethod(saveExpenseRequest.getPaymentMethod())
-                .requiresInvoice(saveExpenseRequest.getRequiresInvoice())
-                .isPaidInFull(saveExpenseRequest.getIsPaidInFull())
-                .installments(saveExpenseRequest.getInstallments())
-                .isRecurring(saveExpenseRequest.getIsRecurring())
-                .recurrenceType(saveExpenseRequest.getRecurrenceType())
-                .vendor(saveExpenseRequest.getVendor())
-                .location(saveExpenseRequest.getLocation())
-                .card(saveExpenseRequest.getCard())
-                .category(saveExpenseRequest.getCategory())
-                .tags(saveExpenseRequest.getTags())
-                .attachments(saveExpenseRequest.getAttachments())
-                .build();
+        try{
+            return Expense.builder()
+                    .name(saveExpenseRequest.getName())
+                    .description(saveExpenseRequest.getDescription())
+                    .amount(saveExpenseRequest.getAmount())
+                    .currency(saveExpenseRequest.getCurrency())
+                    .expenseDate(saveExpenseRequest.getExpenseDate())
+                    .paymentMethod(saveExpenseRequest.getPaymentMethod())
+                    .requiresInvoice(saveExpenseRequest.getRequiresInvoice())
+                    .isPaidInFull(saveExpenseRequest.getIsPaidInFull())
+                    .installments(saveExpenseRequest.getInstallments())
+                    .isRecurring(saveExpenseRequest.getIsRecurring())
+                    .recurrenceType(saveExpenseRequest.getRecurrenceType())
+                    .vendor(saveExpenseRequest.getVendor())
+                    .location(saveExpenseRequest.getLocation())
+                    .card(saveExpenseRequest.getCard())
+                    .category(saveExpenseRequest.getCategory())
+                    .tags(saveExpenseRequest.getTags())
+                    .attachments(saveExpenseRequest.getAttachments())
+                    .build();
+        }
+        catch (Exception e) {
+            log.error("Error occurred while mapping request to expense: {}", e.getMessage(), e);
+            throw new MappingException("Error while mapping request to expense: " + e);
+        }
     }
 }
