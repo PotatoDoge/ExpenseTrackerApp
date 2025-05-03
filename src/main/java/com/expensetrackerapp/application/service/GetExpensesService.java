9786d4954ebee91fb9cedc8af0786d67be0d6c48
Bastian Prado <bastian.prado.v@gmail.com>
2025-05-02 17:19:56 -0700

    ETA-1.2: Refactor exception handling and throwing

--- src/main/java/com/expensetrackerapp/application/service/GetExpensesService.java
+++ src/main/java/com/expensetrackerapp/application/service/GetExpensesService.java
@@ -6,8 +6,12 @@ import com.expensetrackerapp.application.port.out.GetExpensesOutboundPort;
 import com.expensetrackerapp.dto.ExpenseDTO;
 import com.expensetrackerapp.infrastructure.outbound.entities.ExpenseEntity;
 import com.expensetrackerapp.infrastructure.outbound.mappers.ExpenseMapper;
+import com.expensetrackerapp.shared.exceptions.DatabaseInteractionException;
+import com.expensetrackerapp.shared.exceptions.MappingException;
+import jakarta.persistence.PersistenceException;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +26,20 @@ public class GetExpensesService implements GetExpensesUseCase<ExpenseDTO, GetExp
 
     @Override
     public List<ExpenseDTO> getExpenses(GetExpensesFilters filters) {
-        return getExpensesPort.getExpenses(filters).stream().map(expenseMapper::fromEntityToDTO).toList();
+        try{
+            return getExpensesPort.getExpenses(filters).stream().map(expenseMapper::fromEntityToDTO).toList();
+        }
+        catch (MappingException e) {
+            throw new MappingException("Error while mapping expense from entity to DTO: " + e.getClass().getSimpleName());
+        }
+        catch (IllegalArgumentException | PersistenceException | DataAccessException |
+               NullPointerException | ClassCastException e) {
+            log.error("Error occurred while fetching expenses: {} - {}", e.getClass().getSimpleName(), e.getMessage(), e);
+            throw new DatabaseInteractionException("Error while fetching expenses: " + e.getClass().getSimpleName());
+        }
+        catch (Exception e) {
+            log.error("Error occurred while fetching expenses: {} - {}", e.getClass().getSimpleName(), e.getMessage(), e);
+            throw new DatabaseInteractionException("Unhandled error while fetching all entities.");
+        }
     }
 }
--- src/main/java/com/expensetrackerapp/infrastructure/outbound/adapters/GetExpensesRepository.java
+++ src/main/java/com/expensetrackerapp/infrastructure/outbound/adapters/GetExpensesRepository.java
@@ -23,32 +23,20 @@ public class GetExpensesRepository implements GetExpensesOutboundPort<ExpenseEnt
 
     @Override
     public List<ExpenseEntity> getExpenses(GetExpensesFilters filters) {
-        try {
-            List<ExpenseEntity> expenseEntities =
-                    expenseRepository.findAllExpensesByFilters(
-                            filters.getExpenseId(),
-                            filters.getExpenseName(),
-                            filters.getDescription(),
-                            filters.getAmount(),
-                            filters.getCurrency(),
-                            filters.getExpenseDate(),
-                            filters.getRequiresInvoice(),
-                            filters.getInstallments(),
-                            filters.getVendor(),
-                            filters.getLocation(),
-                            filters.getPaidInFull());
-            log.info("Fetched expenses: {}", expenseEntities.size());
-            return expenseEntities;
-        }
-        catch (IllegalArgumentException | PersistenceException | DataAccessException |
-               NullPointerException | ClassCastException e) {
-            log.error("Error occurred while fetching expenses: {}", e.getClass().getSimpleName(), e);
-            throw new DatabaseInteractionException("Error while fetching expenses: " + e.getClass().getSimpleName());
-        }
-        catch (Exception e) {
-            log.error("Unexpected error occurred while fetching expenses", e);
-            throw new DatabaseInteractionException("Unhandled error while fetching all entities.");
-        }
-
+        List<ExpenseEntity> expenseEntities =
+                expenseRepository.findAllExpensesByFilters(
+                        filters.getExpenseId(),
+                        filters.getExpenseName(),
+                        filters.getDescription(),
+                        filters.getAmount(),
+                        filters.getCurrency(),
+                        filters.getExpenseDate(),
+                        filters.getRequiresInvoice(),
+                        filters.getInstallments(),
+                        filters.getVendor(),
+                        filters.getLocation(),
+                        filters.getPaidInFull());
+        log.info("Fetched expenses: {}", expenseEntities.size());
+        return expenseEntities;
     }
 }
