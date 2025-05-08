5b26d421b3e41c1d5fce8e9b64ac43afa7710132
Bastian Prado <bastian.prado.v@gmail.com>
2025-05-07 23:49:42 -0700

    CTA-7.3: Create Category entity

---
+++ src/main/java/com/expensetrackerapp/infrastructure/outbound/entities/CategoryEntity.java
@@ -0,0 +1,29 @@
+package com.expensetrackerapp.infrastructure.outbound.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String icon;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<ExpenseEntity> expenses;
+
+
+}
