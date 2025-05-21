e5f4f7bc712766e3bdbdbf547eebe6210e39c478
Bastian Prado <bastian.prado.v@gmail.com>
2025-05-20 19:58:23 -0700

    CTA-7.4: Add try catch clauses to TagMapping

--- src/main/java/com/expensetrackerapp/infrastructure/outbound/mappers/TagMapper.java
+++ src/main/java/com/expensetrackerapp/infrastructure/outbound/mappers/TagMapper.java
@@ -3,6 +3,7 @@ package com.expensetrackerapp.infrastructure.outbound.mappers;
 import com.expensetrackerapp.domain.model.Tag;
 import com.expensetrackerapp.dto.TagDTO;
 import com.expensetrackerapp.infrastructure.outbound.entities.TagEntity;
+import com.expensetrackerapp.shared.exceptions.MappingException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
@@ -14,38 +15,62 @@ public class TagMapper implements BaseMapper<Tag, TagEntity, TagDTO>{
 
     @Override
     public TagEntity fromPojoToEntity(Tag tag) {
-        return TagEntity
-                .builder()
-                .id(tag.getId())
-                .name(tag.getName())
-                .color(tag.getColor())
-                .build();
+        try{
+            return TagEntity
+                    .builder()
+                    .id(tag.getId())
+                    .name(tag.getName())
+                    .color(tag.getColor())
+                    .build();
+        }
+        catch (Exception ex) {
+            log.error("Error occurred while mapping object to entity: {}", ex.getMessage(), ex);
+            throw new MappingException("Error while mapping object to entity: " + ex);
+        }
     }
 
     @Override
     public TagDTO fromEntityToDTO(TagEntity tagEntity) {
-        return TagDTO
-                .builder()
-                .color(tagEntity.getColor())
-                .name(tagEntity.getName())
-                .id(tagEntity.getId())
-                .build();
+        try{
+            return TagDTO
+                    .builder()
+                    .color(tagEntity.getColor())
+                    .name(tagEntity.getName())
+                    .id(tagEntity.getId())
+                    .build();
+        }
+        catch (Exception ex) {
+            log.error("Error occurred while mapping entity to dto: {}", ex.getMessage(), ex);
+            throw new MappingException("Error while mapping entity to dto: " + ex);
+        }
     }
 
     @Override
     public Tag fromEntityToPOJO(TagEntity tagEntity) {
-        return Tag.builder()
-                .id(tagEntity.getId())
-                .name(tagEntity.getName())
-                .color(tagEntity.getColor())
-                .build();
+        try{
+            return Tag.builder()
+                    .id(tagEntity.getId())
+                    .name(tagEntity.getName())
+                    .color(tagEntity.getColor())
+                    .build();
+        }
+        catch (Exception ex) {
+            log.error("Error occurred while mapping entity to pojo: {}", ex.getMessage(), ex);
+            throw new MappingException("Error while mapping entity to pojo: " + ex);
+        }
     }
 
     public Tag fromMapEntryToTag(Map.Entry<String, String> tagMap) {
-        return Tag.builder()
-                .name(tagMap.getKey().toUpperCase())
-                .color(tagMap.getValue())
-                .build();
+        try{
+            return Tag.builder()
+                    .name(tagMap.getKey().toUpperCase())
+                    .color(tagMap.getValue())
+                    .build();
+        }
+        catch (Exception ex) {
+            log.error("Error occurred while mapping map entry to pojo: {}", ex.getMessage(), ex);
+            throw new MappingException("Error while mapping map entry to pojo: " + ex);
+        }
     }
 
 }
