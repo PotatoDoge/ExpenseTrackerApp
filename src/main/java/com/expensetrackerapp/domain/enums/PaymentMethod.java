308533a94ebcfcc29a2d673274a2eab2a382edcc
Bastian Prado <bastian.prado.v@gmail.com>
2025-05-20 23:11:02 -0700

    PMM-2.1: Add custom serialization and deserialization processes to payment method enum class

--- src/main/java/com/expensetrackerapp/domain/enums/PaymentMethod.java
+++ src/main/java/com/expensetrackerapp/domain/enums/PaymentMethod.java
@@ -1,9 +1,44 @@
 package com.expensetrackerapp.domain.enums;
 
+import com.expensetrackerapp.shared.exceptions.InvalidEnum;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum PaymentMethod {
-    CASH,
-    CARD,
-    BANK_TRANSFER,
-    MOBILE_PAYMENT,
-    OTHER
+    CASH("cash"),
+    CARD("card"),
+    BANK_TRANSFER("bank_transfer"),
+    MOBILE_PAYMENT("mobile_payment"),
+    OTHER("other");
+
+    private final String value;
+
+    PaymentMethod(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Custom deserialization
+     * @param key - enum value
+     * @return enum value
+     * @throws InvalidEnum when an enum value is invalid (not registered in this class)
+     */
+    @JsonCreator
+    public static PaymentMethod fromString(String key) {
+        return Arrays.stream(values())
+                .filter(e -> e.value.equalsIgnoreCase(key))
+                .findFirst()
+                .orElseThrow(() -> new InvalidEnum("Invalid payment method"));
+    }
+
+    /**
+     * Custom serialization
+     * @return enum value
+     */
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
