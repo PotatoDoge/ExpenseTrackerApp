66c90b47a1acbe02509dfa03b946923f7e4dcb66
Bastian Prado <bastian.prado.v@gmail.com>
2025-05-23 23:03:50 -0700

    PMM-2.4: Add constructors to CardDTO

--- src/main/java/com/expensetrackerapp/dto/CardDTO.java
+++ src/main/java/com/expensetrackerapp/dto/CardDTO.java
@@ -1,11 +1,15 @@
 package com.expensetrackerapp.dto;
 
 import com.expensetrackerapp.domain.enums.CardType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardDTO {
 
     private Long id;
